557209b9774a1c28e0cd8e2302ca4fb241663660
Sergiololpro <homerjsimp@yandex.ru>
2024-03-01 15:55:42 +0300

    Date

--- includes/header.php
+++ includes/header.php
@@ -801,7 +801,7 @@
       <div class="modal__close"></div>
       <div class="modal__title">ВНИМАНИЕ, АКЦИЯ!</div>
       <div class="modal__und">Примите участие<br /> в анкетировании и получите<br /> <span class="modal__ttl-colored">дополнительную скидку – 100 руб./тн!</span></div>
-      <div class="modal__und">Срок действия акции: 01.03.2024 – 31.03.2024.</div>
+      <div class="modal__und">Срок действия акции: <div>01.03.2024 – 31.03.2024.</div></div>
       <a href="https://kazan.demidovsteel.ru/pokupatelyu/kontrol-kachestva/" class="modal__button modal__button-dark modal__button-full">ПРИНЯТЬ УЧАСТИЕ</a>
     </div>
   </div>
